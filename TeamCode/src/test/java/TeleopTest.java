010486633547743432e47f4dd6e5484a3dd3bf42
Gabe Walker <gjwalke2@ncsu.edu>
2023-03-10 13:13:40 -0500

    Modified teleop tests to be consistent.

--- TeamCode/src/test/java/TeleopTest.java
+++ TeamCode/src/test/java/TeleopTest.java
@@ -24,29 +24,13 @@ public class TeleopTest {
 
     //Note, the only two things you would need to change to swap Teleops would be GabeTele
     //You could put in whatever Teleop opmode you want here
-    GabeTele tele = new GabeTele();
+    private static GabeTele tele = new GabeTele();
 
-    FakeGamepad gamepad1 = new FakeGamepad();
+    private static FakeGamepad gamepad1 = new FakeGamepad();
 
-    FakeGamepad gamepad2 = new FakeGamepad();
+    private static FakeGamepad gamepad2 = new FakeGamepad();
 
-    Thread Opmode;
-
-
-
-   private void startTeleopLoop(){
-        Opmode = new Thread(new Runnable() {
-            public void run() {
-                while (!Thread.currentThread().isInterrupted()) {
-                    tele.loop();
-                }
-            }
-        });
-
-        Opmode.start();
-    }
-
-    private void updateGamepads(){
+    private void updateGamepads() {
         tele.gamepad1.copy(gamepad1);
         tele.gamepad2.copy(gamepad2);
     }
@@ -65,6 +49,9 @@ public class TeleopTest {
 
         //Initializes Gamepads. Necessary because phone is not running the code, we are
         //Kinda scary right?
+
+        tele.gamepad1 = new FakeGamepad();
+        tele.gamepad2 = new FakeGamepad();
 
     }
 
@@ -106,63 +93,66 @@ public class TeleopTest {
         assertEquals(0, tele.robot.motor4.getPower());
 
         //-----------------------------------------------
+
     }
 
-    @Order(3)
     @Test
     public void moveForwardTest(){
-       startTeleopLoop();
         //Start Teleop Loop here for main loop tests
 
         //Here is where any gamepad commands and tests would go.
 
         gamepad1.setLeftStick(0,1);
-
-        //This code is necessary to update the Teleop GamePad, after each gamepad command
         updateGamepads();
 
+        //Runs the main teleop loop
+        tele.loop();
+
+        //This code is necessary to update the Teleop GamePad, after each gamepad command
+
+        assertEquals(-1, tele.gamepad1.left_stick_y);
+        assertEquals(0, tele.gamepad1.left_stick_x);
 
         assertEquals(1, tele.robot.motor1.getPower());
         assertEquals(1, tele.robot.motor2.getPower());
         assertEquals(1, tele.robot.motor3.getPower());
         assertEquals(1, tele.robot.motor4.getPower());
 
-        Opmode.interrupt();
-
     }
-    @Order(1)
+
     @Test
     public void turnLeftTest() {
-       startTeleopLoop();
 
-        gamepad1.setLeftStick(-1,0);
-
+        gamepad1.setLeftStick(-1, 0);
         updateGamepads();
+
+        //Runs the main teleop loop
+        tele.loop();
+
 
         assertEquals(-1, tele.robot.motor1.getPower());
         assertEquals(1, tele.robot.motor2.getPower());
         assertEquals(-1, tele.robot.motor3.getPower());
         assertEquals(1, tele.robot.motor4.getPower());
 
-        Opmode.interrupt();
     }
 
-    @Order(2)
     @Test
     public void turnRightTest() {
-       startTeleopLoop();
 
-        gamepad1.setLeftStick(1,0);
 
+        gamepad1.setLeftStick(1, 0);
         updateGamepads();
+
+        //Runs the main teleop loop
+        tele.loop();
+
 
         assertEquals(1, tele.robot.motor1.getPower());
         assertEquals(-1, tele.robot.motor2.getPower());
         assertEquals(1, tele.robot.motor3.getPower());
         assertEquals(-1, tele.robot.motor4.getPower());
 
-        Opmode.interrupt();
-        tele.robot.rest();
     }
 
     @BeforeEach
@@ -171,11 +161,6 @@ public class TeleopTest {
         gamepad2 = new FakeGamepad();
         tele.gamepad1 = new FakeGamepad();
         tele.gamepad2 = new FakeGamepad();
-    }
-
-    @AfterAll
-    public void endOpMode(){
-        Opmode.interrupt();
     }
 
 
